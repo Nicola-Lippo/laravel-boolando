971218d60587a0febe9bf5b6f3237bbaf902d980
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-09 15:11:19 +0200

    [UPD] prove passaggio info con layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Template</title>
+    <title>Laravel Boolando</title>
     <!-- Includiamo gli assets con la direttiva vite-->
     @vite('resources/js/app.js')
 </head>
 
 <body>
-    <div class="container">
-        <h1 class="text-center">Template laravello</h1>
-        <img src="{{ Vite::asset('resources/img/foto.png') }}" alt="foto template">
-    </div>
+    @include('shared.header')
+    ciao
+    @include('shared.footer')
 </body>
 
 </html>
